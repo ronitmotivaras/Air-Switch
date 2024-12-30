ea9a36edafde766beb71b320601ac88170e4a735
ronitmotivaras <ronitmotivaras04@gmail.com>
2024-12-30 23:03:29 +0530

    info file add

--- air_switch/lib/main.dart
+++ air_switch/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Air Switch',
       theme: ThemeData(
         // This is the theme of your application.
         //
